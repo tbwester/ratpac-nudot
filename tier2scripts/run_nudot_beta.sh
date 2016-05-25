e1a769cf204f6840763480f058bd36a0db68874a
Taritree Wongjirad <twongjirad@Taritrees-MacBook-Air.local>
2016-05-25 14:07:57 -0400

    added readme

--- tier2scripts/run_nudot_beta.sh
+++ tier2scripts/run_nudot_beta.sh
@@ -3,11 +3,14 @@ JOBID=$1
 NUDOT_OUT=$2
 NEVENTS=$3
 
-# SETUP LOCAL PYTHON
+# SETUP LOCAL PYTHON (We shipped this to the node)
+# This local python is needed to run the gen_macro.py script below
 tar zxvf python2.6.9.tar > /dev/null
 
-# SETUP RAT
+# SETUP RAT (We shipped this to the node)
 tar zxvf rat.tar.gz > /dev/null
+
+# We setup the environment for RAT and the local python we provided
 source ratpac-nudot/tier2scripts/env_condornode_localpython.sh
 
 # GEN MACRO
