9c6f068a8f903f087404af0b91184189b0441695
Thomas Wester <thomas_w@live.com>
2017-07-06 17:24:44 -0400

    working on bash script

--- bo_total_autorun.sh
+++ bo_total_autorun.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
 
-MACFILE='mac/bo_total.mac'
-GDMLFILE='data/bo_total/bo_total.gdml'
-#pi=`echo "4*a(1)" | bc -l`
+## CONFIGURATION ##
+# Source to plate simulation files
+MACFILEPLT='mac/bo_final.mac'
+GDMLFILEPLT='data/bo_final/bo_final.gdml'
 
-for i in `seq 0 1`; #original line 
-#for i in `seq -5 1 5`;
-do
-	#len is variable for starting source position
-	len=`echo "($i*10)-1.9" | bc -l`
-	#len2 is variable for starting source holder position
-	len2=`echo "($i*10)" | bc -l`
+# Plate to PMT simulation files
+MACFILESRC='mac/bo_src.mac'
+GDMLFILESRC='data/bo_src/bo_src.gdml'
 
-	#replace vertex lines in mac file
-        line='21s/.*/\/generator\/pos\/set 0.0 0.0 '$len'/'
-        sed -i "$line" $MACFILE
-        
-        line2='249s/.*/<position name="posSource" unit="mm" x="0" y="0" z="'$len2'"\/>/'
-        sed -i "${line2}" $GDMLFILE
+# Fine-ness, events, etc.
+PLTR=2 #15.24
+DSTEP=1
+NEVENTS=1000
+## END CONFIGURATION ##
 
-	#run rat & python script
-	rat $MACFILE
-        root -l -b -q "analysis/pecount_weight.C($len2)"
-	#analysis/simple_scatter.py $len
-done
+## Generate the weight file
+if [ -f analysis/weights.txt ] && [ "$1" != "new" ] ; then
+    echo "Found weight file."
+else
+    echo "Plate Radius: "$PLTR
+    echo "Step Size: "$DSTEP
+    echo "Events/Point: "$NEVENTS
+    echo "Creating weight file..."
 
+    for i in `seq 0 $DSTEP $PLTR`;
+    do
+        echo "Processing r="$i"cm"
+        len=`echo "$i*10" | bc -l`
+        #replace vertex lines in mac file
+        line='21s/.*/\/generator\/pos\/set '$len' 0.0 300.001/'
+        line2='27s/.*/\/run\/beamOn '$NEVENTS'/'
+        sed -i "$line" $MACFILEPLT
+        sed -i "$line2" $MACFILEPLT
+        #run rat & python script
+        rat -q $MACFILEPLT
+        #root -l -b -q "analysis/pecount_weight.C($len)"
+    done
+fi
+
+## Do source to plate simulation
+
+## Clean up
 rm *.log
