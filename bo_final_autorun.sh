4adf32e739f48dabb405d315c516fe5a72213767
Thomas Wester <thomas_w@live.com>
2017-07-06 14:23:41 -0400

    adding files

--- bo_final_autorun.sh
+++ bo_final_autorun.sh
@@ -3,9 +3,9 @@
 MACFILE='mac/bo_final.mac'
 #pi=`echo "4*a(1)" | bc -l`
 
-#for i in `seq 0 15`;
-array=( 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 )
-for i in "${array[@]}"
+for i in `seq 0 15`;
+#array=( 0 1 )
+#for i in "${array[@]}"
 do
 	len=`echo "$i*10" | bc -l`
 
