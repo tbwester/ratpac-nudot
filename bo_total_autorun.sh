8730526bc61fe5593091c1c6527b61bea26bde15
Thomas Wester <thomas_w@live.com>
2017-09-12 15:59:11 -0400

    \"switch to 36.8\"

--- bo_total_autorun.sh
+++ bo_total_autorun.sh
@@ -21,7 +21,7 @@ NEVENTSSRC=1000000
 
 # Source holder position
 # originally meant to be a list, but probably won't work if more than 1 number
-DISTLIST=( 9.7 )
+DISTLIST=( -6.8 )
 
 ## END CONFIGURATION ##
 
